8a4dfbd56e90baee84f1dcf72df8b0eec809187b
enigmajess <jessicajones99362@gmail.com>
2022-03-20 13:02:40 -0700

    Fully integrated api for daily forecast

--- src/index.js
+++ src/index.js
@@ -30,29 +30,43 @@ displayDate.innerHTML = `${day} ${month} ${date}, ${year}`;
 let h4 = document.querySelector("h4");
 h4.innerHTML = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+
+  return days[day];
+};
+
+
 function displayProjectedForcast(response) {
-  console.log(response.data.daily)
+  let dailyForecast = response.data.daily;
   let forecastElement = document.querySelector("#projected-forecast");
-
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
+  
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
   let forecastHTML = `<div  class ="row">`;
 
-  days.forEach(function (day) {
-    forecastHTML =
+  dailyForecast.forEach(function (forecastDay, index) {
+
+    if (index < 6) {
+      forecastHTML =
       forecastHTML +
       `<div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img
-            src="http://openweathermap.org/img/wn/50d@2x.png"
-            alt=""
-            width="42"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
            />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
         </div>
       </div>`;
+    }
+    
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -61,7 +75,7 @@ function displayProjectedForcast(response) {
 
 function getForecast (coordinates) {
   let key = `4f8353f322c9f415161732592106f878`;
-  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}`
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=imperial`
   console.log(coordinates);
 
   axios.get(apiURL).then(displayProjectedForcast);
@@ -87,8 +101,6 @@ function cityTemp(response) {
     "wind: " + Math.round(response.data.wind.speed) + " mph";
 
   getForecast(response.data.coord);
-
-  displayProjectedForcast();
 }
 
 function citySearch(event) {
@@ -149,7 +161,6 @@ function currentTemp(response) {
     "wind: " + Math.round(response.data.wind.speed) + " mph";
   
   getForecast(response.data.coord);
-  displayProjectedForcast();
 }
 
 function coorPosition(position) {
